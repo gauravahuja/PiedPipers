fa1e9a4904d7366287010225de4875248e987cd4
Gaurav Ahuja <gauravahuja9@gmail.com>
2014-09-10 10:58:08 -0400

    Working

--- PiedPipers/src/piedpipers/gaurav/Player.java
+++ PiedPipers/src/piedpipers/gaurav/Player.java
@@ -23,6 +23,7 @@ public class Player extends piedpipers.sim.Player {
 	static double yt;
 	static double quadrant_height = 0;
 	static int state = 0;
+	static boolean check_for_rats = false;
 	
 	static Point destination = new Point();
 
@@ -37,8 +38,8 @@ public class Player extends piedpipers.sim.Player {
 
 	    xl = dimension/2 + PIPER_INFLUENCE;
 	    xr = dimension - PIPER_INFLUENCE;
-	    yb = quadrant_height*id + PIPER_INFLUENCE;
-	    yt = quadrant_height*id + quadrant_height - PIPER_INFLUENCE;
+	    yt = quadrant_height*id + quadrant_height/2;
+	    yb = quadrant_height*id + quadrant_height - PIPER_INFLUENCE;
 
 	    destination.x = dimension/2;
 	    destination.y = dimension/2;
@@ -46,12 +47,42 @@ public class Player extends piedpipers.sim.Player {
 	    System.out.println("Intializing");
 	    System.out.println("Dimension: " + dimension);
 	    System.out.println("Quadrant Height: " + quadrant_height);	
-	    System.out.printf("(xl=%f, yb=%f)->(xr=%f, yt=%f)\n", xl, yb, xr, yt);
+	    System.out.printf("(xl=%f, yt=%f)->(xr=%f, yb=%f)\n", xl, yt, xr, yb);
 	    System.out.printf("Destination: (%f, %f)\n", destination.x, destination.y);
 	}
 
 	static double distance(Point a, Point b) {
 		return Math.sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
+	}
+
+	public void check_rats(Point[] pipers, Point[] rats)
+	{
+		System.out.printf("Piper: %d, check_rats, state: %d\n", id, state);
+		double [] distances = new double[nrats];
+		for (int r = 0; r < nrats; r++)
+		{
+			distances[r] = dimension;
+			for(int p = 0; p < npipers; p++)
+			{
+				distances[r] = Math.min(distance(pipers[p], rats[r]), distances[r]);
+
+			}
+		}
+		
+		boolean all_rats_m = true;
+		for (int r = 0; r < nrats; r++)
+		{
+			if (distances[r] >= PIPER_INFLUENCE)
+			{
+				all_rats_m = false;
+				break;
+			}
+		}
+		if (all_rats_m)
+		{
+			state = 3;
+		}
+		System.out.printf("Piper: %d, check_rats end, state: %d\n", id, state);
 	}
 
 	public Point move(Point[] pipers, Point[] rats) {
@@ -69,7 +100,7 @@ public class Player extends piedpipers.sim.Player {
 		double dist = distance(current, destination);
 
 
-		System.out.printf("Piper: %d, (xl=%f, yb=%f)->(xr=%f, yt=%f)\n", id, xl, yb, xr, yt);
+		System.out.printf("Piper: %d, (xl=%f, yt=%f)->(xr=%f, yb=%f)\n", id, xl, yt, xr, yb);
 		System.out.printf("Piper: %d, State: %d\n", id, state);
 		System.out.printf("Piper: %d, Current: (%f, %f)\n", id, current.x, current.y);
 		System.out.printf("Piper: %d, Destination: (%f, %f)\n", id, destination.x, destination.y);
@@ -93,28 +124,51 @@ public class Player extends piedpipers.sim.Player {
 		}
 		else
 		{
-			System.out.printf("Piper: %d, State Space Change, Old Destination: (%f, %f)\n", id, destination.x, destination.y);
+			System.out.printf("Piper: %d, State Space Change, Old State: %d, Old Destination: (%f, %f), check_rats: %b\n", id, state, destination.x, destination.y, check_for_rats);
 			switch(state)
 			{
 				case 0:
+					this.music = false;
+					check_for_rats = false;
 					destination.x = xr;
-					destination.y = yb;
+					destination.y = yt;
 					state += 1;
 					break;
 				case 1:
 					this.music = true;
 					destination.x = xl;
-					destination.y = yb;
+					destination.y = yt;
 					state += 1;
+					if(check_for_rats)
+					{
+						check_rats(pipers, rats);
+					}
 					break;
 				case 2:
 					this.music = true;
+					check_for_rats = true;
 					destination.x = xr;
-					destination.y = yb;
+					destination.y = yt;
 					state -= 1;
+					if(check_for_rats)
+					{
+						check_rats(pipers, rats);
+					}
 					break;
+				case 3:
+					this.music = true;
+					check_for_rats = false;
+					destination.x = dimension/2;
+					destination.y = dimension/2;
+					state = 4;
+					break;
+				case 4:
+					this.music = true;
+					destination.x = 0;
+					destination.y = dimension/2;
+					state = 0;
 			}
-			System.out.printf("Piper: %d, State Space Change, New Destination: (%f, %f)\n", id, destination.x, destination.y);
+			System.out.printf("Piper: %d, State Space Change, New State: %d, New Destination: (%f, %f), check_rats: %b\n", id, state, destination.x, destination.y, check_for_rats);
 		}
 		current.x += ox;
 		current.y += oy;
