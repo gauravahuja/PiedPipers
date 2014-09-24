82874be36bb862c9c1017078073088d0a95310b3
powerrmt <powerrmt@gmail.com>
2014-09-24 00:32:15 -0400

    Added getFurthestRat functionality (but it's not great)

--- PiedPipers/src/piedpipers/predictionStrategyQuadFlip/Player.java
+++ PiedPipers/src/piedpipers/predictionStrategyQuadFlip/Player.java
@@ -26,6 +26,8 @@ public class Player extends piedpipers.sim.Player {
   static int current_state = 0;
   static boolean check_for_rats = false;
 
+  static final int FURTHEST_RAT_STATE = 1;
+
 
   static Point destination = new Point();
 
@@ -57,7 +59,7 @@ public class Player extends piedpipers.sim.Player {
     quadrant_height = dimension/npipers;
     quadrant_width = quadrant_height/2;
 
-    apply_new_state(0);
+    apply_new_state(-1);
 
     System.out.printf("Intializing Piper: %d\n", id);
     System.out.println("Quadrant Height: " + quadrant_height);	
@@ -70,16 +72,19 @@ public class Player extends piedpipers.sim.Player {
 
   public int get_new_state()
   {
-    int new_state = -1;
+    int new_state = -2;
     switch(current_state)
     {
-      case 0:
+      case -1:
         // Check if we're going out to get stragglers
-        new_state = 1;
+        new_state = 0;
         break;
-      case 1:
+      case 0:
         new_state = 2;
         break;
+     //case FURTHEST_RAT_STATE:
+     //  new_state = 2;
+     //  break;
       case 2:
         Point closest = getClosestPredictedRat();
         if(closest != null) {
@@ -103,7 +108,7 @@ public class Player extends piedpipers.sim.Player {
       case 7:
         new_state = 7;
         if(!emptyQuadrant(this.ratsNow)) {
-          new_state = 0;
+          new_state = -1;
           break;
         }
         break;
@@ -115,16 +120,26 @@ public class Player extends piedpipers.sim.Player {
   {
     switch(new_state)
     {
-      case 0:
+      case -1:
         // Target gate at the beginning
         destination.x = dimension/2;
         destination.y = dimension/2;
         music = false;
         break;
-      case 1:
-        // Target gate at the beginning
+      case 0:
+        //Squeeze past the gate a bit to avoid invalid moves
         destination.x = dimension - MIN_PIPER_SEPARATION;
-        destination.y = (quadrant_height * id) + quadrant_height/2;
+        if (id == npipers - 1)
+          destination.y = (dimension + quadrant_height  * id)/2; //Ensure that we cover the whole field
+        else
+          destination.y = quadrant_height * id + quadrant_height/2;
+
+        //System.out.println("Piper " + id + " has quad bounds " + (destination.y - quadrant_height/2) + " and " + (destination.y + quadrant_height/2));
+        music = false;
+        break;
+      case FURTHEST_RAT_STATE:
+        current_state = new_state; //need this for furthest rat check
+        destination = getFurthestPredictedRat();
         music = false;
         break;
       case 2:
@@ -154,6 +169,48 @@ public class Player extends piedpipers.sim.Player {
     current_state = new_state;
   }
 
+  private Point getFurthestPredictedRat()
+  {
+    // Returns furthest predicted rat within a quadrant
+    // Used to determine starting location of a piper in a game.
+
+    //How many ticks does it take to get to far right?
+    Point rightmost = new Point();
+    Point gate = destination;//where we are now
+    rightmost.x = dimension - MIN_PIPER_SEPARATION;
+    rightmost.y = quadrant_height * id + quadrant_height/2;
+
+    //System.out.println("Gate: " + gate.x + ", " + gate.y + " rightmost: " + rightmost.x + ", " + rightmost.y);
+    int cur_tick = (int) (distance(gate,rightmost) / pspeed);
+
+    //System.out.println("It will take " + cur_tick + " ticks to get " + distance(gate,rightmost) + "m away");
+
+    //Keep track of furthest rat we find
+    Point max_Rat = new Point(gate.x,gate.y);
+
+    //
+    for (; cur_tick > 0; cur_tick -= 1 )
+    {
+      Point[] futureRatPositions = Predict.getFutureRatPositions(cur_tick, dimension, this.ratsNow, this.thetas, this.pipers, this.piperMusic);
+
+      ArrayList<Point> ratsInRange = ratsInRange(this.pipers[id], cur_tick, futureRatPositions);
+      //System.out.println("We found " + ratsInRange.size() + " rats in range");
+      if (ratsInRange.size() > 0){
+        for (int i = 0; i < ratsInRange.size() ; i ++){
+          if (distance(ratsInRange.get(i),this.pipers[id]) > distance(max_Rat,this.pipers[id])){ 
+            max_Rat = ratsInRange.get(i);
+          }
+        }
+        break;
+      }
+    }
+    //System.out.println("Piper " +id + " is going " + cur_tick * pspeed +" distance to point " + max_Rat.x + ", " + max_Rat.y);
+    this.targetRatTicsRemaining = cur_tick;
+    return max_Rat;
+
+    
+
+  }
   private Point getClosestPredictedRat()
   {
     // Returns closest predicted rat
@@ -184,8 +241,10 @@ public class Player extends piedpipers.sim.Player {
           continue;
         }
         this.targetRatTicsRemaining = cur_tick;
-        if (flag)
+        if (flag){
           use_quad = true;
+          flag = false;
+        }
         return ratsInRange.get(0);
       }
     }
@@ -197,8 +256,15 @@ public class Player extends piedpipers.sim.Player {
   {
     ArrayList<Point> ratsInRange = new ArrayList<Point>();
     double rat_distance;
-    
+    double speed;
 
+
+    if (piperMusic[id])
+      speed = mpspeed;
+    else
+      speed = pspeed;
+    //System.out.println("Can be at " + (radius * speed) + PIPER_INFLUENCE +" going at a speed of " + speed);
+    
     for(int i = 0; i < rats.length; i++)
     {
       if(rats[i].x < dimension / 2.0 || rats[i].y < 0) {
@@ -207,35 +273,20 @@ public class Player extends piedpipers.sim.Player {
       rat_distance = helper.distance(current,rats[i]);
 
       // Check if the rat is within range of where the piper can be in "radius" ticks
-      if(rat_distance < (radius * mpspeed) + PIPER_INFLUENCE)
+      if(rat_distance < (radius * speed) + PIPER_INFLUENCE)
       {
         if ((!use_quad && rat_distance > PIPER_INFLUENCE) || quadrantContainsPoint(id, rats[i])) {
+          //System.out.println("GOt rat at point " + rats[i].x + ", " + rats[i].y);
           this.targetRatIndex = i;
           ratsInRange.add(rats[i]);
-          return ratsInRange; // Just returning closest one for faster computation
+          if (current_state != FURTHEST_RAT_STATE){
+            //System.out.println("Error because current state is " + current_state + " but check is " + FURTHEST_RAT_STATE);
+            return ratsInRange; // Just returning closest one for faster computation
+          }
         }
       }
     }
     return ratsInRange;
-  }
-
-  public int getHighestPiperCloseTo(Point point,int radius){
-    for (int i = npipers-1; i > -1; i--)
-      if (withinRangeOfPiper(i,point,radius))
-        return i;
-    return -1;
-  }
-
-  public boolean withinRangeOfMultiplePipers( Point point, int radius){
-    int count = 0;
-    for (int i = 0; i < npipers; i ++){
-      if (withinRangeOfPiper(i,point,radius))
-        count++;
-      if (count >= 2)
-        return true;
-    }
-    return false;
-    
   }
 
   //Return true if this piper's quadrant doesn't have any free rats
@@ -351,6 +402,8 @@ public class Player extends piedpipers.sim.Player {
       apply_new_state(new_state);
       o = calc_offset(current, destination);
     }
+
+    //System.out.println("Now state for piper " + id + " is " + current_state);
 
     current.x += o.x;
     current.y += o.y;
